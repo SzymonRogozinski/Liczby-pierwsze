615d7ee81e8640d57df75c2de4aeefa4e8c91022
SzymonRogozinski <pokeszymon.pl@gmail.com>
2022-12-15 10:51:46 +0100

    Przyspieszenie generacji liczb pierwszy, dodanie obliczania położenia ich na spirali

--- Liczby pierwsze/main.c
+++ Liczby pierwsze/main.c
@@ -1,41 +1,77 @@
 #include <stdio.h>
+#include <math.h>
 
-#define size 500
+#define size 1000
+
 
 //Czy podzielna przez pewne liczby
 int ifDevide(int a) {
-	if (a % 7 == 0 || a % 11 == 0)
+	/*if (a % 7 == 0 || a % 11 == 0)
 		return 1;
-	else
+	else*/
 		return 0;
 }
 
 //Czy liczba pierwsza
 int ifFirst(int a) {
 	int j;
-	int max_j = a / 2+1;
-	for (j = 2; j < max_j; j++)
+	int max_j = sqrt(a);
+	for (j = 2; j <= max_j; j++)
 		if (a % j == 0)
 			return 0;
 	return 1;
 }
 
+//Przesunięcie
+void up(int *x,int *y){
+	(*y)--;
+}
+
+void down(int *x,int *y){
+	(*y)++;
+}
+
+void left(int *x,int *y){
+	(*x)--;
+}
+
+void right(int *x,int *y){
+	(*x)++;
+}
+
 void main(int argc, char** argv) {
 	FILE* F = fopen("Liczby.txt","w+");
-	int i=2; //Uzyskane liczby
-	int number = 2;//Kolejne liczby
-	int iteration = size * size + 1;
-	fprintf(F, "\n");//Zapisanie jedynki
-	while (i < iteration) {
-		if(!ifDevide(number)){
-			if (ifFirst(number)){
-				fprintf(F, ".\n");
-			}
-			else
-				fprintf(F,"\n");
-			i++;
-		}
+	fprintf(F, "Czy_pierwsza;x;y\n");//nagłówek
+	int x=size/2;
+	int y=x;
+	void (*function[])(int*,int*)={right,down,left,up};
+	fprintf(F,"%d;%d;%d\n",0,x,y);
+	x++;
+	int number = 1; //Obecna liczba
+	int way=0; //przebyta droga od ostatniego zakrętu
+	short direction=1; //Który kierunek
+	float max_way=1; //Ile może przebyć, przed zmianą kierunku
+	long iteration=size*size+1; //z ilu wyników ma się składać próba
+	long i=1;
+	while(i<iteration){
 		number++;
+		if(ifDevide(number))
+			continue;
+		
+		fprintf(F,"%d;%d;%d\n",ifFirst(number),x,y);
+		function[direction](&x,&y);
+		way++;
+		i++;
+		//Czy następuję zmiana kierunku
+		if(way<max_way)
+			continue;
+		//Zmiana kierunku
+		way=0;
+		max_way+=0.5;
+		if(direction==3)
+			direction=0;
+		else
+			direction++;
 	}
 	fclose(F);
 }
